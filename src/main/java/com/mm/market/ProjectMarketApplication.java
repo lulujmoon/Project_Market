a3facdfcb581b93f7c7457defb173c79bf799757
lulujmoon <80494527+lulujmoon@users.noreply.github.com>
2021-05-24 17:14:53 +0900

    init

---
+++ src/main/java/com/mm/market/ProjectMarketApplication.java
@@ -0,0 +1,13 @@
+package com.mm.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMarketApplication.class, args);
+	}
+
+}
