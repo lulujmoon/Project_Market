64a391b3394d1e73dffe39f85f351b8377a37058
fly5824 <fly5148@naver.com>
2021-05-26 14:54:15 +0900

    f

--- src/main/java/com/mm/market/config/WebSecurityConfig.java
+++ src/main/java/com/mm/market/config/WebSecurityConfig.java
@@ -6,19 +6,23 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	
+	
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/css/**","/script/**");
+		web.ignoring().antMatchers("/css/**","/script/**","/img/**","/resources/**");
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers("/**").permitAll();
+		.antMatchers("/login","/").permitAll();
 	}
 
 
