12893d34a8443b4c9efb8ba4a8fa2ee098d920d4
fly5824 <fly5148@naver.com>
2021-05-26 17:49:39 +0900

    authen

--- src/main/java/com/mm/market/member/MemberController.java
+++ src/main/java/com/mm/market/member/MemberController.java
@@ -1,6 +1,12 @@
 package com.mm.market.member;
 
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +24,35 @@ public class MemberController {
 	public String getLogin()throws Exception{
 		return "member/memberLogin";
 	}
+	
+	@GetMapping("memberLoginFail")
+	public String loginFail()throws Exception{
+		return "redirect:/member/memberLogin";
+	}
+	
+	@GetMapping("memberLoginResult")
+	public String memberLoginResult(HttpSession session, Authentication auth2)throws Exception{
+		
+		Enumeration<String> en = session.getAttributeNames();
+		
+		while(en.hasMoreElements()) {
+			System.out.println("attribute Name:+"+en.nextElement());
+		}
+		
+		Object obj = session.getAttribute("SPRING_SECURITY_CONTEXT");
+		
+		SecurityContextImpl sc = (SecurityContextImpl)obj;
+		
+		Authentication auth= sc.getAuthentication();
+		
+		System.out.println("obj : "+obj);
+		
+		System.out.println("login 성공");
+		
+		return "redirect:/";
+		
+	}
+	
 	
 	
 	
--- src/main/java/com/mm/market/member/MemberVO.java
+++ src/main/java/com/mm/market/member/MemberVO.java
@@ -1,9 +1,12 @@
 package com.mm.market.member;
 
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.Data;
@@ -18,29 +21,41 @@ public class MemberVO implements UserDetails{
 	private String email;
 	private Date joinDate;
 	
+	private boolean enabled;
+	
+	//role
+	private List<RoleVO> roles;
+	
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return null;
+		//계정이 가지고 있는 권한을 목록으로 리턴
+		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+		
+		for(RoleVO roleVO:this.roles) {
+			authorities.add(new SimpleGrantedAuthority(roleVO.getRoleName()));
+		}
+		return authorities;
 	}
+	
+	
 	@Override
 	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
-		return false;
+		// 계정이 만료되지 않았는지 
+		return true;
 	}
 	@Override
 	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
-		return false;
+		// 계정이 잠겨있지 않은지
+		return true;
 	}
 	@Override
 	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
-		return false;
+		// 비밀번호가 만료되지 않았는지
+		return true;
 	}
 	@Override
 	public boolean isEnabled() {
-		// TODO Auto-generated method stub
-		return false;
+		// 계정이 활성화(사용가능)인지
+		return this.enabled;
 	}
 }
