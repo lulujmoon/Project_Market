8be72322ddd3a931d624ef38e6e17705f99080d9
shlee39 <39shlee@gmail.com>
2021-05-27 10:46:50 +0900

    Revert "Merge branch 'sihyun' into product"
    This reverts commit 50aa098e729e4df908a427888dbd1206685686ba, reversing
    changes made to fbf115380918ca8d0f001d90f9ceb177c6b6f6c3.

--- src/main/java/com/mm/market/product/ProductMapper.java
+++ src/main/java/com/mm/market/product/ProductMapper.java
@@ -4,10 +4,5 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface ProductMapper {
-	
-	//insert
-	public int setInsert(ProductVO productVO) throws Exception;
-	//update
-	public int setUpdate(ProductVO productVO)throws Exception;
-	
+
 }
--- src/main/java/com/mm/market/product/ProductService.java
+++ src/main/java/com/mm/market/product/ProductService.java
@@ -1,21 +1,8 @@
 package com.mm.market.product;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
-	
-	@Autowired
-	private ProductMapper productMapper;
 
-	//insert
-	public int setInsert(ProductVO productVO) throws Exception{
-		return productMapper.setInsert(productVO);
-	}
-	//update
-	public int setUpdate(ProductVO productVO) throws Exception{
-		return 0;
-	}
 }
-
