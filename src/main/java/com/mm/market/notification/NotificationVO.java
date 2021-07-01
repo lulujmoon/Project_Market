76c496c4b9a88e1861b0b9ce98bbf8f07fa082c7
shlee39 <39shlee@gmail.com>
2021-07-01 15:56:53 +0900

    상품번호넣기

--- src/main/java/com/mm/market/notification/NotificationVO.java
+++ src/main/java/com/mm/market/notification/NotificationVO.java
@@ -14,6 +14,7 @@ public class NotificationVO {
 	private String notiContent;
 	private String notiReadChk; // 1: 읽지않음, 0: 읽음
 	private String productNum;
+	private String notiFrom; //1: 가격제안 0: 채팅
 	
 	private int unread;
 	private String username;
