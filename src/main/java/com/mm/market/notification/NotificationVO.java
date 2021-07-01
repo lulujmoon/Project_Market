cef1f73f5df46f9a93f389bcd63cef9e45d0c17f
thunder-e <pinkpear0114@naver.com>
2021-07-01 23:42:56 +0900

    commit

--- src/main/java/com/mm/market/notification/NotificationVO.java
+++ src/main/java/com/mm/market/notification/NotificationVO.java
@@ -14,7 +14,7 @@ public class NotificationVO {
 	private String notiContent;
 	private String notiReadChk; // 1: 읽지않음, 0: 읽음
 	private Long productNum; //
-	private Boolean notiFrom; //1: 채팅메시지 알림 0: 가격제안
+	private boolean notiFrom; //1: 채팅메시지 알림 0: 가격제안
 	
 	private int unread;
 	private String username;
